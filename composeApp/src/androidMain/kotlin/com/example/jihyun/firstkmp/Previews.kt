f37182a4d3a4fe5a57b8cd458f0902501c7418f5
Jihyun-Park0 <jihyun.park.private@gmail.com>
2024-09-20 22:48:30 +0900

    :lipstick: Create Login Screen
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Examples ====
    # :bug: Fix importer bug #12334
    # :sparkles: Add Heads up feature.
    # :memo: Fix contrib.md [SKIP CI]
    
    # ==== Tips ====
    # You can instruct CI to skip individual commits by including [CI SKIP] or [SKIP CI] in the commit message. Note this is case-insensitive.
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    
    # ==== Emojis ====
    # 🔥  :fire: - Removing code or files.
    # 💄  :lipstick: - Updating the UI and style files.
    # 🎨  :art: - Improving structure / format of the code.
    # 🚑  :ambulance: - Critical hotfix.
    # 🐛  :bug: - Fixing a bug.
    # 💚  :green_heart: - Fixing CI Build.
    # ✨  :sparkles: - Introducing new features.
    # ⚡️  :zap: - Improving performance.
    # 🔨  :hammer: - Refactoring code.
    # 🚨  :rotating_light: - Removing linter warnings.
    # 🚧  :construction: - Work in progress.
    # ⬆️  :arrow_up: - Upgrading dependencies.
    # 🔒  :lock: - Fixing security issues.
    # ✏️  :pencil2: - Fixing typos.
    # 📝  :memo: - Writing docs.
    # 💥  :boom: - Introducing breaking changes.
    # 👌  :ok_hand: - Updating code due to code review changes.
    
    # ==== More emojis ====
    # 🚀  :rocket: - Deploying stuff.
    # ✅  :white_check_mark: - Adding tests.
    # 🔖  :bookmark: - Releasing / Version tags.
    # ⬇️  :arrow_down: - Downgrading dependencies.
    # 👷  :construction_worker: - Adding CI build system.
    # 📈  :chart_with_upwards_trend: - Adding analytics or tracking code.
    # ➖  :heavy_minus_sign: - Removing a dependency.
    # 🐳  :whale: - Work about Docker.
    # ➕  :heavy_plus_sign: - Adding a dependency.
    # 🔧  :wrench: - Changing configuration files.
    # 🌐  :globe_with_meridians: - Internationalization and localization.
    # 💩  :hankey: - Writing bad code that needs to be improved.
    # ⏪  :rewind: - Reverting changes.
    # 🔀  :twisted_rightwards_arrows: - Merging branches.
    # 📦  :package: - Updating compiled files or packages.
    # 👽  :alien: - Updating code due to external API changes.
    # 🚚  :truck: - Moving or renaming files.
    # 📄  :page_facing_up: - Adding or updating license.
    # 🍱  :bento: - Adding or updating assets.
    # ♿️  :wheelchair: - Improving accessibility.
    # 💡  :bulb: - Documenting source code.
    # 🍻  :beers: - Writing code drunkenly.
    # 💬  :speech_balloon: - Updating text and literals.
    # 🗃  :card_file_box: - Performing database related changes.

--- composeApp/src/androidMain/kotlin/com/example/jihyun/firstkmp/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/example/jihyun/firstkmp/Previews.kt
@@ -2,6 +2,7 @@ package com.example.jihyun.firstkmp
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jihyun.firstkmp.screen.DetailScreen
 import com.example.jihyun.firstkmp.screen.ListScreen
 import com.example.jihyun.firstkmp.screen.LoginScreen
 import com.example.jihyun.firstkmp.theme.FirstKMPTheme
@@ -25,5 +26,16 @@ fun LoginScreen_Preview() {
 fun ListScreen_Preview() {
     FirstKMPTheme {
         ListScreen()
+    }
+}
+
+/**
+ * 詳細画面 Preview
+ */
+@Preview(showSystemUi = false)
+@Composable
+fun DetailScreen_Preview() {
+    FirstKMPTheme {
+        DetailScreen()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/example/jihyun/firstkmp/screen/DetailScreen.kt
@@ -0,0 +1,158 @@
+package com.example.jihyun.firstkmp.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import firstkmp.composeapp.generated.resources.Res
+import firstkmp.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+
+/**
+ * 詳細画面
+ *
+ * @param modifier 装飾
+ * @param id 識別式
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    id: Int = 1
+) {
+    Scaffold(
+        topBar = {
+            // ヘッダー
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    // 戻るボタン
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "",
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                    }
+                },
+                title = {
+                    // タイトル
+                    Text(
+                        text = "Detail",
+                        style = MaterialTheme.typography.displayMedium
+                    )
+                }
+            )
+        },
+        modifier = modifier
+    ) { paddingValues ->
+
+        // ボディ
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+
+            item {
+                // 画像
+                Card(
+                    colors = CardDefaults.cardColors(
+                        MaterialTheme.colorScheme.surface
+                    ),
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(Res.drawable.compose_multiplatform),
+                        contentDescription = "item $id",
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                }
+            }
+
+            item {
+                // 詳細
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                    modifier = Modifier
+                        .background(
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    DetailListItem(title = "ID", description = id.toString())
+                    HorizontalDivider()
+                    DetailListItem(title = "title1", description = "description1")
+                    HorizontalDivider()
+                    DetailListItem(title = "title2", description = "description2")
+                }
+            }
+        }
+
+    }
+}
+
+/**
+ * 詳細画面一覧アイテム
+ *
+ * @param modifier 装飾
+ * @param title タイトル
+ * @param description 説明
+ */
+@Composable
+fun DetailListItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        // タイトル
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .weight(0.3f)
+        )
+
+        // 説明
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .weight(0.7f)
+        )
+    }
+}
